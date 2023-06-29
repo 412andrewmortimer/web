09483868a3e154203fd6348a11bbfa9c6b371450
Andrew Mortimer <mortimera@upmc.edu>
2023-06-29 16:06:45 -0400

    Fuss with Shared UI

--- libs/shared-ui/src/lib/components/home/home.component.ts
+++ libs/shared-ui/src/lib/components/home/home.component.ts
@@ -10,19 +10,7 @@ import { NavigationComponent } from '../navigation/navigation.component';
   selector: 'web-home',
   standalone: true,
   imports: [CommonModule, NavigationComponent],
-  template: `
-  <web-navigation></web-navigation>
-  <div class="hero min-h-screen bg-base-200">
-    <div class="hero-content text-center">
-      <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Hello there</h1>
-        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        <button class="btn btn-primary">Get Started</button>
-      </div>
-    </div>
-  </div>
-  `,
-  styleUrls: ['./home.component.scss'],
+  template: `<web-navigation></web-navigation>`,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
