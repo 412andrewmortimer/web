ff0484063a3bb6d03300160cb35b9c506d4c6222
Andrew Mortimer <andrcmdr@protonmail.com>
2024-09-01 14:26:41 +0000

    Migrate Nx to v19 and Angular to v18

--- apps/parknitro/webpack.config.js
+++ apps/parknitro/webpack.config.js
@@ -1,7 +1,11 @@
 const { withModuleFederation } = require('@nx/angular/module-federation');
 const config = require('./module-federation.config');
 <<<<<<< HEAD
+<<<<<<< HEAD
 module.exports = withModuleFederation(config, { dts: false });
 =======
 module.exports = withModuleFederation(config);
 >>>>>>> 0165f27 (Add third remote: Park Nitro)
+=======
+module.exports = withModuleFederation(config, { dts: false });
+>>>>>>> 5fc4f2f (Migrate Nx to v19 and Angular to v18)
--- apps/parknitro/webpack.prod.config.js
+++ apps/parknitro/webpack.prod.config.js
@@ -2,9 +2,13 @@ const { withModuleFederation } = require('@nx/angular/module-federation');
 const config = require('./module-federation.config');
 module.exports = withModuleFederation({
 <<<<<<< HEAD
+<<<<<<< HEAD
   dts: false,
 =======
 >>>>>>> 0165f27 (Add third remote: Park Nitro)
+=======
+  dts: false,
+>>>>>>> 5fc4f2f (Migrate Nx to v19 and Angular to v18)
   ...config,
   /*
    * Remote overrides for production.
